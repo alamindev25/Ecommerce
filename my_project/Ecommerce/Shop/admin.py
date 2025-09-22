233535a51889df3c9b708a73528668e7d761f135
alaminsql <alamin88.ime@gmail.com>
2025-09-22 10:59:52 +0600

    initial commit

---
+++ my_project/Ecommerce/Shop/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import (
+     Customer,
+    Product,
+
+)
+ 
+# Register your models here.
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display =['id', 'user', 'name', 'division','district','thana','villorroad','zipcode']
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display =['id', 'title','selling_price','discounted_price','description','brand','category','product_image']
---
+++ my_project/Ecommerce/Shop/urls.py
@@ -0,0 +1,22 @@
+
+from django.urls import path
+from Shop import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm,MyPasswordChangeForm,MyPasswordReset
+
+urlpatterns = [
+    path('', views.ProductView.as_view(), name="home"),
+    path('product-detail/<int:pk>',views.ProductDetail.as_view(),name="product-detail"),
+    path('lehenga/', views.lehenga, name='lehenga'),
+    path('lehenga/<slug:data>',views.lehenga, name='lehengaitem'),
+    path('profile/',views.profile, name='profile'),
+    path('registration/',views.CustomerRegistrationView.as_view(),name='customerregistration'),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name='Shop/login.html',authentication_form=LoginForm),name='login'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name ='Shop/passwordchange.html', form_class= MyPasswordChangeForm, success_url='/passwordchangedone/'), name="passwordchange"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name='Shop/passwordchangedone.html'), name="passwordchangedone"),
+
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name='Shop/password_reset.html', form_class=MyPasswordReset),name='password_reset'),
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
