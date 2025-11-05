d79009483f2c8cf87c2a0d7f0288eeb1c0e5f68e
AL Amin Islam <alamin88.ime@gmail.com>
2025-11-05 09:32:05 +0600

    Delete TodoApp directory

--- TodoApp/TodoApp/notebook/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'completed', 'created')
